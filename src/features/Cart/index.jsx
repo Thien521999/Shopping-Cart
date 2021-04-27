17e7939881b108bec7b9db47f7fc263284483f57
Thien521999 <trhoangthien1999@gmail.com>
2021-04-27 15:19:31 +0700

    Update Cart

--- src/features/Cart/index.jsx
+++ src/features/Cart/index.jsx
@@ -1,36 +1,53 @@
-import { Box, Container, Grid } from '@material-ui/core';
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { removeFromCart } from './cartSlice';
+import { Box, Button, Container, Grid, makeStyles, Paper } from '@material-ui/core';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
+import CartItem from './components/CartItem';
 import CartPay from './components/CartPay';
-import InfoCart from './components/InfoCart';
 import { cartItemCountSelector, cartTotalSelector } from './selectors';
 
 CartFeature.propTypes = {
 
 };
 
+const useStyles = makeStyles((theme) => ({
+    left: { width: "910px" },
+}));
+
 function CartFeature(props) {
-    const dispatch = useDispatch();
+    const classes = useStyles();
 
+    //const dispatch = useDispatch();
+    const history = useHistory();
+
+    // Tong so san pham
     const cartTotal = useSelector(cartTotalSelector);
+
+    // So san pham trong gio hang
     const numberItemInCart = useSelector(cartItemCountSelector);
+
+    // Mảng cac san pham,
     const infoProduct = useSelector(state => state.cart.cartItems);
-    console.log(typeof infoProduct);
+    //console.log(infoProduct);
 
-    const [productList, setProductList] = useState(infoProduct);
-    console.log(productList);
+    //const [productList, setProductList] = useState(infoProduct);
+    //console.log(productList[0].id);
 
-    const handleDelete = (item, idx) => {
-        console.log(item, idx);
-        const action = removeFromCart(idx);
-        dispatch(action);
+    // const handleDelete = (item, idx) => {
+    //     console.log(item, idx);
 
-        const newProduct = [...productList];
-        newProduct.splice(idx, 1);
+    //     const action = removeFromCart(idx);
+    //     dispatch(action);
 
-        setProductList(newProduct);
-    }
+    //     const newProduct = [...productList];
+    //     newProduct.splice(idx, 1);
+
+    //     setProductList(newProduct);
+    // }
+
+    const handleClickContinueBuy = () => {
+        history.push('/');
+    };
 
     return (
         <Box>
@@ -38,7 +55,39 @@ function CartFeature(props) {
                 <Grid container spacing={1} style={{ marginTop: "6px" }} className="product">
 
                     {/* Left column */}
-                    <InfoCart infoProduct={productList} cartTotal={cartTotal} numberItemInCart={numberItemInCart} onDelete={handleDelete} />
+                    <Grid item className={classes.left} >
+                        <Box component="div">
+                            <h2 className="title">
+                                GIỎ HÀNG
+                                <span className="product-count"> ({numberItemInCart} sản phẩm)</span>
+                            </h2>
+                        </Box>
+
+                        {
+                            infoProduct.length > 0 && (
+                                infoProduct.map((product, idx) => (
+                                    <CartItem
+                                        key={product.id}
+                                        product={product}                                    
+                                    />
+                                ))
+                            )
+                        }
+
+                        {
+                            infoProduct.length <= 0 && (
+                                <Paper elevation={0} >
+                                    <Box component="div" className="empty">
+                                        <img src="https://salt.tikicdn.com/desktop/img/mascot@2x.png" alt="" className="empty__img" />
+                                        <p className="empty__note">Không có sản phẩm nào trong giỏ hàng của bạn</p>
+                                        <Button variant="contained" color="secondary" className="empty__btn" size="medium" onClick={handleClickContinueBuy}>
+                                            Tiếp tục mua sắm
+                                        </Button>
+                                    </Box>
+                                </Paper>
+                            )
+                        }
+                    </Grid>
 
                     {/* Right column */}
                     <CartPay cartTotal={cartTotal} />
